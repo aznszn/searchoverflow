0f757ff026d4d253048c85955d71bf6762943a6b
aznszn <akadri.bscs20seecs@student.nust.edu.pk>
2021-12-09 21:36:47 +0500

    cleaned up code

--- build_lexicon.cpp
+++ build_lexicon.cpp
@@ -1,21 +1,23 @@
-//
-// Created by HP on 08/12/2021.
-//
 #include "bits/stdc++.h"
 #include "stemminglib/english_stem.h"
 #include "utils/fetch_table.h"
 #include "utils/parseHTML.h"
+
 #define WORDS_IN_FILE 500
+#define MAX_WORD_LEN 17
+#define TITLE_IMP 2
+#define BODY_IMP 1
+#define TAG_IMP 0
+
 
 using namespace std;
 
 void buildLexicon(unordered_map<wstring, int> &, vector<vector<wstring>>, vector<vector<wstring>>, int&);
-void buildForwardIndex(unordered_map<wstring, int> &, unordered_map<wstring, int> &, int, wstring);
-void utilLexiconFunction(unordered_map<wstring, int> &, unordered_map<wstring, int> &, wstringstream *, int &);
-int getAnswer(int &, vector<vector<wstring>> &, wstring);
+void buildForwardIndex(unordered_map<wstring, int> &, unordered_map<wstring, int> &, int, const wstring&);
+void utilLexiconFunction(unordered_map<wstring, int> &, unordered_map<wstring, int> &, wstringstream&, int &);
+int getAnswer(int &, vector<vector<wstring>> &, const wstring&);
 
-//int overallCount = 0;
-//wofstream *array_fi = new wofstream[500];
+
 //TODO: remove global
 auto array_fi = new vector<wofstream>;
 int main() {
@@ -25,25 +27,26 @@ int main() {
 
     int i = 0;
 
-        wifstream questions("../dataset/questions_100k.csv");
-        vector<vector<wstring>> questions_table = fetch_table(questions);
-        cout << "questions table fetched" << endl;
+    wifstream questions("../dataset/questions_100k.csv");
+    vector<vector<wstring>> questions_table = fetch_table(questions);
+    cout << "questions table fetched" << endl;
 
-        wifstream answers("../dataset/answers_100k.csv");
-        vector<vector<wstring>> answers_table = fetch_table(answers);
-        cout << "answers table fetched" << endl;
+    wifstream answers("../dataset/answers_100k.csv");
+    vector<vector<wstring>> answers_table = fetch_table(answers);
+    cout << "answers table fetched" << endl;
 
-        parseHTML(questions_table, 6);
-        cout << "questions HTML parsed" << endl;
-        //parseHTML(answers_table, 5);
-        cout << "answers HTML parsed" << endl;
+    parseHTML(questions_table, 6);
+    cout << "questions HTML parsed" << endl;
+    parseHTML(answers_table, 5);
+    cout << "answers HTML parsed" << endl;
 
 
-        buildLexicon(lexicon, questions_table, answers_table, i);
+    buildLexicon(lexicon, questions_table, answers_table, i);
 
-        for(auto& x : *array_fi)
-            x.close();
+    for (auto &x: *array_fi)
+        x.close();
 
+    delete array_fi;
     //writing lexicon to file
     for (auto &x: lexicon)
         lexicon_file << x.first << L"," << x.second << "\n";
@@ -52,32 +55,26 @@ int main() {
 }
 
 void buildLexicon(unordered_map<wstring, int> &lexicon, vector<vector<wstring>> questions, vector<vector<wstring>> answers, int& i) {
-    int TITLE_IMP = 2;
-    int BODY_IMP = 1;
-
     int j = 0;
     int k = 0;
 
-    //wofstream forwardIndex("../data_structures/forwardIndex.txt");
-    while(j < questions.size()) {
+    while (j < questions.size()) {
         unordered_map<wstring, int> storesCount;
         unordered_map<wstring, int> storesQACount;
 
-        wstringstream *row_stream = new wstringstream(questions[j][5]);
+        auto row_stream = wstringstream(questions[j][5]);
         utilLexiconFunction(lexicon, storesCount, row_stream, i);
         buildForwardIndex(lexicon, storesCount, TITLE_IMP, questions[j][0]);
-
-        row_stream = new wstringstream(questions[j][6]);
+        row_stream = wstringstream(questions[j][6]);
         utilLexiconFunction(lexicon, storesQACount, row_stream, i);
 
-        while(1){
+        while (1) {
             int ansFound = getAnswer(k, answers, questions[j][0]);
 
-            if (ansFound == -1){
+            if (ansFound == -1) {
                 break;
             }
-            //cout << "Answers" << k << endl;
-            wstringstream *row_stream = new wstringstream(answers[ansFound][5]);
+            row_stream = wstringstream(answers[ansFound][5]);
             utilLexiconFunction(lexicon, storesQACount, row_stream, i);
         }
         buildForwardIndex(lexicon, storesQACount, BODY_IMP, questions[j][0]);
@@ -85,12 +82,12 @@ void buildLexicon(unordered_map<wstring, int> &lexicon, vector<vector<wstring>> 
     }
 }
 
-void utilLexiconFunction(unordered_map<wstring, int> &lexicon, unordered_map<wstring, int> &storesCount, wstringstream *row_stream, int &i){
+void utilLexiconFunction(unordered_map<wstring, int> &lexicon, unordered_map<wstring, int> &storesCount, wstringstream& row_stream, int &i){
     wstring word;
     stemming::english_stem<> stem;
     wchar_t c;
 
-    while (*row_stream >> noskipws >> c) {
+    while (row_stream >> noskipws >> c) {
         switch (c) {
             case '.':
             case ',':
@@ -116,23 +113,16 @@ void utilLexiconFunction(unordered_map<wstring, int> &lexicon, unordered_map<wst
             case '\n':
             case '\"':
                 if (!word.empty()) {
-                    //wcout << word << "\t";
                     stem(word);
-                    //wcout << word << "\n";
-                    if (word.length() <= 17 && !lexicon.count(word)) {
-                        if(i % (WORDS_IN_FILE + 1) == 0) {
-                            array_fi->push_back(wofstream("../data_structures/f_index/" + to_string(i /(WORDS_IN_FILE + 1)) + ".txt", ios::out));
+                    if (word.length() <= MAX_WORD_LEN && !lexicon.count(word)) {
+                        if (i % (WORDS_IN_FILE + 1) == 0) {
+                            array_fi->push_back(wofstream(
+                                    "../data_structures/f_index/" + to_string(i / (WORDS_IN_FILE + 1)) + ".txt",
+                                    ios::out));
                         }
                         lexicon[word] = i++;
-                        //++overallCount;
-                        storesCount[word] = 1;
                     }
-                    else if (word.length() <= 17 && storesCount.count(word)) {
-                        storesCount[word] += 1;
-                    }
-                    else if (word.length() <= 17){
-                        storesCount[word] = 1;
-                    }
+                    storesCount[word] = storesCount.count(word) + 1;
                     word.clear();
                 }
                 break;
@@ -143,44 +133,29 @@ void utilLexiconFunction(unordered_map<wstring, int> &lexicon, unordered_map<wst
     }
     if (!word.empty()) {
         stem(word);
-        if (word.length() <= 17 && !lexicon.count(word)){
-            if(i % (WORDS_IN_FILE + 1) == 0) {
-                array_fi->push_back(wofstream("../data_structures/f_index/" + to_string(i / (WORDS_IN_FILE + 1)) + ".txt", ios::out));
+        if (word.length() <= MAX_WORD_LEN && !lexicon.count(word)) {
+            if (i % (WORDS_IN_FILE + 1) == 0) {
+                array_fi->push_back(wofstream(
+                        "../data_structures/f_index/" + to_string(i / (WORDS_IN_FILE + 1)) + ".txt",
+                        ios::out));
             }
             lexicon[word] = i++;
-            //++overallCount;
-            storesCount[word] = 1;
         }
-        else if (word.length() <= 17 && storesCount.count(word)) {
-            storesCount[word] += 1;
-        }
-        else if (word.length() <= 17){
-            storesCount[word] = 1;
-        }
+        storesCount[word] = storesCount.count(word) + 1;
         word.clear();
     }
 }
 
-void buildForwardIndex(unordered_map<wstring, int> &lexicon, unordered_map<wstring, int> &storesCount, int importance, wstring id){
+void buildForwardIndex(unordered_map<wstring, int> &lexicon, unordered_map<wstring, int> &storesCount, int importance, const wstring& id){
     for(auto &x: storesCount){
         int wordId = lexicon[x.first];
-        //wofstream forwardIndex("../data_structures/forwardIndex" + to_string(wordId / 1000) + ".txt", ios::app);
-
-        array_fi->at(wordId / 501) << id << L"," << lexicon[x.first] << L"," << x.second << L"," << importance << endl;
-        //forwardIndex.close();
+        array_fi->at(wordId / (WORDS_IN_FILE + 1)) << id << L"," << lexicon[x.first] << L"," << x.second << L"," << importance << endl;
     }
 }
 
-int getAnswer(int &k, vector<vector<wstring>>& answers, wstring id){
-    while (k < answers.size()){
-        if (answers[k][3] == id){
-            return k++;
-        }
-        else{
-            return -1;
-        }
-    }
-    return -1;
+inline int getAnswer(int &k, vector<vector<wstring>>& answers, const wstring& id){
+    if(k < answers.size() && answers[k][3] == id)
+        return k++;
+    else
+        return -1;
 }
-
-
